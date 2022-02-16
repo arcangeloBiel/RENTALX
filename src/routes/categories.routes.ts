a7028814b6e2bec358924b2f176199b1460c8875
joao <joao.ribeiro@havan.com.br>
2022-02-16 05:45:26 -0300

    criando uplod de imagem com multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer  from 'multer';
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
+
+const uplod = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request,response);
@@ -10,6 +15,12 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/imports", uplod.single("file"), (request, response) => {
+  const { file } = request;
+  console.log("File ::", file);
+   return response.send();
 });
 
 
